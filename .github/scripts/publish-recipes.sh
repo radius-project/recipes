13befdc4c6da537a8daf55debd0cf6bfd0cd0288
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-04-04 17:08:34 -0700

    Add path logic

--- .github/scripts/publish-recipes.sh
+++ .github/scripts/publish-recipes.sh
@@ -1,12 +1,20 @@
 #! /bin/bash
 # This script is used to publish recipes to ACR
 
+if [[ ! -z $BICEP_PATH ]]
+then
+    BICEP_EXECUTABLE="$BICEP_PATH/rad-bicep"
+fi
+
 echo "## Recipes published to $ACR_HOST" >> $GITHUB_STEP_SUMMARY
 for RECIPE in $(find . -type f -name "*.bicep")
 do
+    # Get the recipe name and directory name
+    # ./azure/redis-azure.bicep -> redis-azure and azure, respectively
     export FILE_NAME=$(basename $RECIPE | cut -d. -f1)
     export DIR_NAME=$(dirname $RECIPE | cut -d/ -f2)
+
     echo "Publishing $FILE_NAME Recipe from $RECIPE"
     echo "- `$ACR_HOST/recipes/$DIR_NAME/$FILE_NAME:$REL_VERSION`" >> $GITHUB_STEP_SUMMARY
-    ../../rad-bicep publish $RECIPE --target br:$ACR_HOST/recipes/$DIR_NAME/$FILE_NAME:$REL_VERSION
+    $BICEP_EXECUTABLE publish $RECIPE --target br:$ACR_HOST/recipes/$DIR_NAME/$FILE_NAME:$REL_VERSION
 done
