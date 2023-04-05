4992c08740bc2c3a227d23c0d1b802811473402a
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-04-05 12:18:18 -0700

    Switch to positional arguments

--- .github/scripts/delete-recipes.sh
+++ .github/scripts/delete-recipes.sh
@@ -1,6 +1,28 @@
 #! /bin/bash
-# This script is used to publish recipes to ACR
 
+# Fail immedietly if any command fails
+set -e
+
+# Get command line arguments
+ACR_HOST=$1
+RECIPE_VERSION=$2
+
+# Print usage information
+function print_usage() {
+    echo "Usage: $0 <ACR_HOST>"
+    echo "  Deletes all recipes in the repository from the Azure Container Registry. Requires you to be logged into Azure via az login."
+    echo "  ACR_HOST: Host name of the Azure Container Registry. For example, myregistry.azurecr.io."
+    echo "  RECIPE_VERSION: Version of the recipe to publish. For example, 1.0"
+}
+
+# Verify that the required arguments are present
+if [ -z "$ACR_HOST" ] || [ -z "$RECIPE_VERSION" ]; then
+    echo "Missing required arguments"
+    print_usage
+    exit 1
+fi
+
+# Login to Azure Container Registry
 az acr login --name $ACR_HOST
 
 for RECIPE in $(find . -type f -name "*.bicep")
@@ -11,5 +33,5 @@ do
     export DIR_NAME=$(dirname $RECIPE | cut -d/ -f2)
 
     echo "Deleting $ACR_HOST/recipes/$DIR_NAME/$FILE_NAME:1.0"
-    az acr repository delete --name $ACR_HOST --image "recipes/$DIR_NAME/$FILE_NAME:1.0" --yes
+    az acr repository delete --name $ACR_HOST --image "recipes/$DIR_NAME/$FILE_NAME:$RECIPE_VERSION" --yes
 done
--- .github/scripts/publish-recipes.sh
+++ .github/scripts/publish-recipes.sh
@@ -1,9 +1,29 @@
 #! /bin/bash
-# This script is used to publish recipes to ACR
 
-if [[ ! -z $BICEP_PATH ]]
-then
-    BICEP_EXECUTABLE="$BICEP_PATH/rad-bicep"
+# Fail immedietly if any command fails
+set -e
+
+# Get command line arguments
+BICEP_PATH=$1
+ACR_HOST=$2
+RECIPE_VERSION=$3
+
+BICEP_EXECUTABLE="$BICEP_PATH/rad-bicep"
+
+# Print usage information
+function print_usage() {
+    echo "Usage: $0 <BICEP_PATH> <ACR_HOST>"
+    echo "  Publishes all recipes in the repository to the Azure Container Registry. Requires you to be logged into Azure via az login."
+    echo "  BICEP_PATH: Path to the bicep executable. For example, ~/.rad/bin"
+    echo "  ACR_HOST: Host name of the Azure Container Registry. For example, myregistry.azurecr.io."
+    echo "  RECIPE_VERSION: Version of the recipe to publish. For example, 1.0"
+}
+
+# Verify that the required arguments are present
+if [ -z "$BICEP_PATH" ] || [ -z "$ACR_HOST" ] || [ -z "$RECIPE_VERSION" ]; then
+    echo "Missing required arguments"
+    print_usage
+    exit 1
 fi
 
 echo "## Recipes published to $ACR_HOST" >> $GITHUB_STEP_SUMMARY
