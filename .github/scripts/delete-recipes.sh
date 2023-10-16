4cfa8f80dc904e2e5e8661b1c8c533440e9ab945
willdavsmith <willdavsmith@gmail.com>
2023-10-16 12:16:04 -0700

    PR

--- .github/scripts/delete-recipes.sh
+++ .github/scripts/delete-recipes.sh
@@ -5,6 +5,7 @@ set -e
 
 # Get command line arguments
 GHCR_ORG=$1
+GHCR_PATH=$2
 RECIPE_VERSION=$2
 
 # Print usage information
@@ -12,20 +13,15 @@ function print_usage() {
     echo "Usage: $0 <GHCR_ORG> <RECIPE_VERSION>"
     echo "  Deletes all recipes in the repository from the GitHub Container Registry. Requires you to be logged into GitHub."
     echo "  GHCR_ORG: Organization name of the GitHub Container Registry. For example, radius-project"
+    echo "  GHCR_PATH: Path name for Recipe storage. For example, recipes"
     echo "  RECIPE_VERSION: Version of the recipe to publish. For example, 1.0"
 }
 
 # Verify that the required arguments are present
-if [ -z "$GHCR_ORG" ] || [ -z "$RECIPE_VERSION" ]; then
+if [ -z "$GHCR_ORG" ] || [ -z "$GHCR_PATH" ] || [ -z "$RECIPE_VERSION" ]; then
     echo "Missing required arguments"
     print_usage
     exit 1
-fi
-
-# We create output that's intended to be consumed by the GitHub Action summary. If we're
-# not running in a GitHub Action, we'll just silence the output.
-if [[ -z "$GITHUB_STEP_SUMMARY" ]]; then
-    GITHUB_STEP_SUMMARY=/dev/null
 fi
 
 for RECIPE in $(find . -type f -name "*.bicep")
@@ -35,10 +31,10 @@ do
     export FILE_NAME=$(basename $RECIPE | cut -d. -f1)
     export DIR_NAME=$(dirname $RECIPE | cut -d/ -f2)
 
-    echo "Deleting ghcr.io/$GHCR_ORG/recipes/$DIR_NAME/$FILE_NAME:$RECIPE_VERSION"
+    echo "Deleting ghcr.io/$GHCR_ORG/$GHCR_PATH/$DIR_NAME/$FILE_NAME:$RECIPE_VERSION"
     gh api \
         --method DELETE \
         -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        /orgs/$GHCR_ORG/packages/container/recipes/$DIR_NAME/$FILE_NAME:$RECIPE_VERSION
+        /orgs/$GHCR_ORG/packages/container/$GHCR_PATH/$DIR_NAME/$FILE_NAME:$RECIPE_VERSION
 done
--- .github/scripts/publish-recipes.sh
+++ .github/scripts/publish-recipes.sh
@@ -5,18 +5,20 @@ set -e
 
 # Get command line arguments
 GHCR_ORG=$1
-RECIPE_VERSION=$2
+GHCR_PATH=$2
+RECIPE_VERSION=$3
 
 # Print usage information
 function print_usage() {
     echo "Usage: $0 <GHCR_ORG> <RECIPE_VERSION>"
     echo "  Publishes all recipes in the repository to the GitHub Container Registry. Requires you to be logged into GitHub"
     echo "  GHCR_ORG: Organization name of the GitHub Container Registry. For example, radius-project"
+    echo "  GHCR_PATH: Path name for Recipe storage. For example, recipes"
     echo "  RECIPE_VERSION: Version of the recipe to publish. For example, 1.0"
 }
 
 # Verify that the required arguments are present
-if [ -z "$GHCR_ORG" ] || [ -z "$RECIPE_VERSION" ]; then
+if [ -z "$GHCR_ORG" ] || [ -z "$GHCR_PATH" ] || [ -z "$RECIPE_VERSION" ]; then
     echo "Missing required arguments"
     print_usage
     exit 1
@@ -28,14 +30,14 @@ if [[ -z "$GITHUB_STEP_SUMMARY" ]]; then
     GITHUB_STEP_SUMMARY=/dev/null
 fi
 
-echo "## Recipes published to ghcr.io/$GHCR_ORG" >> $GITHUB_STEP_SUMMARY
+echo "## Recipes published to ghcr.io/$GHCR_ORG/$GHCR_PATH" >> $GITHUB_STEP_SUMMARY
 for RECIPE in $(find . -type f -name "*.bicep")
 do
     # Get the platform (file) name and resource (directory) name
     export FILE_NAME=$(basename $RECIPE | cut -d. -f1) # rediscaches
     export DIR_NAME=$(dirname $RECIPE | cut -d/ -f2)   # dev
 
-    echo "Publishing $DIR_NAME/$FILE_NAME to ghcr.io/$GHCR_ORG from $RECIPE"
-    echo "- ghcr.io/$GHCR_ORG/recipes/$DIR_NAME/$FILE_NAME:$RECIPE_VERSION" >> $GITHUB_STEP_SUMMARY
-    rad bicep publish --file $RECIPE --target "br:ghcr.io/$GHCR_ORG/recipes/$DIR_NAME/$FILE_NAME:$RECIPE_VERSION"
+    echo "Publishing $DIR_NAME/$FILE_NAME to ghcr.io/$GHCR_ORG/$GHCR_PATH from $RECIPE"
+    echo "- ghcr.io/$GHCR_ORG/$GHCR_PATH/$DIR_NAME/$FILE_NAME:$RECIPE_VERSION" >> $GITHUB_STEP_SUMMARY
+    rad bicep publish --file $RECIPE --target "br:ghcr.io/$GHCR_ORG/$GHCR_PATH/$DIR_NAME/$FILE_NAME:$RECIPE_VERSION"
 done
